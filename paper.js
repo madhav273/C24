ede320e17b5a2a64a562d2d46a8d06c2368ff417
madhav273 <69422223+madhav273@users.noreply.github.com>
2021-06-22 15:00:03 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -4,6 +4,7 @@ class Paper {
         this.x = x; this.y = y; this.r = r
         this.body = Bodies.circle(this.x, this.y, this.r / 2, options)
         World.add(world, this.body);
+        this.image = loadImage("paper.png");
     }
     display() {
         var paperpos = this.body.position;
@@ -11,6 +12,9 @@ class Paper {
         translate(paperpos.x, paperpos.y);
         rectMode(CENTER)
         strokeWeight(3); fill(255, 0, 255)
-        ellipse(0, 0, this.r, this.r); pop()
+        //ellipse(0, 0, this.r, this.r); 
+        imageMode(CENTER); image(this.image, 0,0,this.r, this.r)
+        pop()
+        
     }
 }
